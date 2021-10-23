b394003a9e3f2ee915fe838f60db0f66de0def99
Erwin Alan Frías Martínez <alan.frias@erwcode.com>
2021-10-23 17:04:30 -0500

    postcss setting up :gear::zap:
    + postcss-loader allows you to process css with postcss.
    + postcss-preset-env lets you convert modern css into something most browser can understand.

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,7 @@ module.exports = {
         use: [
           { loader: MiniCssExtractPlugin.loader },
           { loader: 'css-loader' },
+          { loader: 'postcss-loader' },
           { loader: 'sass-loader', options: { sourceMap: true} }
         ]
       },
